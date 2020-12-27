5242b3b5bdb447f2613e8e6173c900aafb052e3a
石俊峰 <shijf_private@163.com>
2020-12-27 17:57:42 +0800

    创建数据表

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func initDB() {
 	config := mysql.Config{
 		User:                 "root",
 		Passwd:               "",
-		Addr:                 "127.0.0.1:3360",
+		Addr:                 "127.0.0.1:3306",
 		Net:                  "tcp",
 		DBName:               "goblog",
 		AllowNativePasswords: true,
@@ -54,8 +54,21 @@ func initDB() {
 	// 尝试链接，失败会报错
 	db.Ping()
 
+	fmt.Print(err)
+
 	checkError(err)
 
+}
+
+func createTables() {
+	createArticlesSQL := `CREATE TABLE IF NOT EXISTS articles(
+    id bigint(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    body longtext COLLATE utf8mb4_unicode_ci
+); `
+
+	_, err := db.Exec(createArticlesSQL)
+	checkError(err)
 }
 
 func checkError(err error) {
@@ -172,6 +185,7 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 
 func main() {
 	initDB()
+	createTables()
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 
