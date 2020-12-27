032e0723166b2ca04672710a217f5121521e8590
石俊峰 <shijf_private@163.com>
2020-12-27 10:33:02 +0800

    读取请求数据

--- main.go
+++ main.go
@@ -37,35 +37,40 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
-    if err != nil {
-        // 解析错误，这里应该有错误处理
-        fmt.Fprint(w,  "请提供正确的数据！")
-        return
-    }
+	if err != nil {
+		// 解析错误，这里应该有错误处理
+		fmt.Fprint(w, "请提供正确的数据！")
+		return
+	}
 
-    title := r.PostForm.Get("title")
+	title := r.PostForm.Get("title")
 
-    fmt.Fprintf(w, "POST PostForm: %v <br>", r.PostForm)
-    fmt.Fprintf(w, "POST Form: %v <br>", r.Form)
-    fmt.Fprintf(w, "title 的值为: %v", title)
+	fmt.Fprintf(w, "POST PostForm: %v <br>", r.PostForm)
+	fmt.Fprintf(w, "POST Form: %v <br>", r.Form)
+	fmt.Fprintf(w, "title 的值为: %v", title)
+
+	fmt.Fprintf(w, "r.Form 中 title 的值为: %v <br>", r.FormValue("title"))
+	fmt.Fprintf(w, "r.PostForm 中 title 的值为: %v <br>", r.PostFormValue("title"))
+	fmt.Fprintf(w, "r.Form 中 test 的值为: %v <br>", r.FormValue("test"))
+	fmt.Fprintf(w, "r.PostForm 中 test 的值为: %v <br>", r.PostFormValue("test"))
 }
 
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
 	html := `
-	<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <title>创建文章 —— 我的技术博客</title>
 </head>
 <body>
-    <form action="%s" method="post">
+    <form action="%s?test=data" method="post">
         <p><input type="text" name="title"></p>
         <p><textarea name="body" cols="30" rows="10"></textarea></p>
         <p><button type="submit">提交</button></p>
     </form>
 </body>
 </html>
-	`
+`
 	storeURL, _ := router.Get("articles.store").URL()
 	fmt.Fprintf(w, html, storeURL)
 }
@@ -102,5 +107,5 @@ func main() {
 	// 中间件：强制内容类型为 HTML
 	router.Use(forceHTMLMiddleware)
 
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
+	http.ListenAndServe(":3000", router)
 }
